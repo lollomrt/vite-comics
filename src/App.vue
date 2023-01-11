5bb6eb10831fbbf045ec9c57ccc71caf0a425585
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-11 16:22:52 +0100

    Modifica ubicazione cartella styles, ora è figlia di src

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppIconList from './components/AppIconList.vue'
+import AppFooter from './components/AppFooter.vue'
 export default {
-
+  components: {
+    AppHeader,
+    AppMain,
+    AppIconList,
+    AppFooter
+  }
 }
 </script>
 
 <template lang="">
-  <div>
-    
-  </div>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppIconList></AppIconList>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@ export default {
 
 <template lang="">
     <div>
-        
+        <h1>Ciao</h1>
     </div>
 </template>
 
